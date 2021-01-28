39b187b24fa4fb1ce8860f5dc87405e3d486965b
ErezKatsman <erezkatsman2311@gmail.com>
2021-01-28 02:22:54 +0200

    passed 7 tests
    i made sort button, addaed sort function and print function and replaced the print function when the function should be.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const textInput = document.getElementById("text-input");
 const addButton = document.getElementById("add-button");
+const sortButton = document.getElementById("sort-button");
 const viewSection = document.getElementById("view-section");
+
 let taskArr = [];
 
 if (localStorage.getItem("taskArr") === null) {
@@ -8,25 +10,7 @@ if (localStorage.getItem("taskArr") === null) {
 } else {
   let priority = document.getElementById("priority-selector").value;
   taskArr = JSON.parse(localStorage.getItem("taskArr"));
-  for (let i = 0; i < taskArr.length; i++) {
-    const todoContainer = document.createElement("div");
-    todoContainer.className = "todo-container";
-
-    const priorityDiv = document.createElement("div");
-    priorityDiv.innerHTML = taskArr[i].priority;
-    priorityDiv.className = "todo-priority";
-
-    const createdAtDiv = document.createElement("div");
-    createdAtDiv.innerHTML = taskArr[i].date;
-    createdAtDiv.className = "todo-created-at";
-
-    const textDiv = document.createElement("div");
-    textDiv.innerHTML = taskArr[i].task;
-    textDiv.className = "todo-text";
-
-    todoContainer.append(priorityDiv, createdAtDiv, textDiv);
-    viewSection.appendChild(todoContainer);
-  }
+  printViewSection(taskArr);
 }
 
 const countText = document.getElementById("counter");
@@ -58,8 +42,14 @@ addButton.addEventListener("click", () => {
   todoContainer.append(priorityDiv, createdAtDiv, textDiv);
   viewSection.appendChild(todoContainer);
 
+  taskArr.push({
+    priority: priority,
+    date: new Date().toDateString(),
+    task: task,
+  });
+
   let newArr = JSON.parse(localStorage.getItem("taskArr"));
-  console.log(newArr);
+
   newArr.push({
     priority: priority,
     date: new Date().toDateString(),
@@ -70,5 +60,40 @@ addButton.addEventListener("click", () => {
   countText.innerText = `${
     JSON.parse(localStorage.getItem("taskArr")).length
   } tasks`;
-  console.log(priority);
 });
+
+sortButton.addEventListener("click", () => {
+  let newArr = [];
+  for (let j = 5; j >= 1; j--) {
+    for (let i = 0; i < taskArr.length; i++) {
+      if (Number(taskArr[i].priority) === j) {
+        newArr.push(taskArr[i]);
+      }
+    }
+  }
+  console.log(newArr);
+  printViewSection(newArr);
+});
+
+function printViewSection(arr) {
+  viewSection.innerText = "";
+  for (let i = 0; i < arr.length; i++) {
+    const todoContainer = document.createElement("div");
+    todoContainer.className = "todo-container";
+
+    const priorityDiv = document.createElement("div");
+    priorityDiv.innerHTML = arr[i].priority;
+    priorityDiv.className = "todo-priority";
+
+    const createdAtDiv = document.createElement("div");
+    createdAtDiv.innerHTML = arr[i].date;
+    createdAtDiv.className = "todo-created-at";
+
+    const textDiv = document.createElement("div");
+    textDiv.innerHTML = arr[i].task;
+    textDiv.className = "todo-text";
+
+    todoContainer.append(priorityDiv, createdAtDiv, textDiv);
+    viewSection.appendChild(todoContainer);
+  }
+}
