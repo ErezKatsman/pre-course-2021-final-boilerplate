c846a7c94de4875e867a88332a4ac79307a2ad0e
ErezKatsman <erezkatsman2311@gmail.com>
2021-02-16 14:15:12 +0200

    finished my task
    1. added spinner
    2. made my json bin saving data work with callback.

--- src/main.js
+++ src/main.js
@@ -17,258 +17,230 @@ if (localStorage.getItem("mode") === "dark") {
   theme.href = "dark.css";
   darkLight.innerText = "light";
 }
-async function main() {
-  const textInput = document.getElementById("text-input"); //text input
-  const addButton = document.getElementById("add-button"); //add button
-  const sortButton = document.getElementById("sort-button"); // sort button
-  const viewSection = document.getElementById("view-section"); //the view section
-  const countText = document.getElementById("counter"); // the counter tasks
-  const searchButton = document.getElementById("search-button"); //the search button
+const textInput = document.getElementById("text-input"); //text input
+const addButton = document.getElementById("add-button"); //add button
+const sortButton = document.getElementById("sort-button"); // sort button
+const viewSection = document.getElementById("view-section"); //the view section
+const countText = document.getElementById("counter"); // the counter tasks
+const searchButton = document.getElementById("search-button"); //the search button
+let taskArr = [];
+const loader = document.getElementById("loader");
 
-  // Gets data from persistent storage by the given key and returns it
-  function getPersistent() {
-    const promise = fetch(
-      "https://api.jsonbin.io/v3/b/602a718799ac3873a349c2b4/latest"
-    );
-    const loadedData = promise.then((response) => {
-      return response.json();
-    });
-    const data = loadedData.then((result) => {
-      return result.record["my-todo"];
-    });
-    return data;
+document.addEventListener("DOMConetentLoaded", getPersistent());
+
+//event of add button
+addButton.addEventListener("click", () => {
+  const pin = document.createElement("div"); //making the pin with random color
+  pin.className = "pin";
+  var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  pin.style.backgroundColor = `#${randomColor}`;
+  const task = textInput.value;
+  if (!task.trim()) {
+    //if the input is empty - get out from the function
+    alert("Please enter your task and prioirity");
+    return;
   }
+  textInput.value = ""; //make the input empty again
+  let priority = document.getElementById("priority-selector").value;
+  const todoContainer = document.createElement("div");
+  todoContainer.className = "todo-container";
+  const color = colorTask(priority); //paint the task backgorund div for each priority
+  todoContainer.style.backgroundColor = color;
+  const priorityDiv = document.createElement("div"); //making priority div
+  priorityDiv.innerHTML = priority;
+  priorityDiv.className = "todo-priority";
+  const createdAtDiv = document.createElement("div"); //making date div
+  createdAtDiv.innerHTML = new Date().toDateString();
+  createdAtDiv.className = "todo-created-at";
+  const textDiv = document.createElement("div"); //created task div
+  textDiv.innerHTML = task;
+  textDiv.className = "todo-text";
+  todoContainer.append(pin, priorityDiv, createdAtDiv, textDiv);
+  viewSection.appendChild(todoContainer);
+  taskArr.push({
+    //push the task to the main arr
+    priority: priority,
+    date: new Date().toDateString(),
+    text: task,
+  });
+  setPersistent(taskArr); //set the taskArr in the JSON bin
+  countText.innerText = taskArr.length;
+});
 
-  // Saves the given data into persistent storage by the given key.
-  // Returns 'true' on success.
-  async function setPersistent(data) {
-    await fetch("https://api.jsonbin.io/v3/b/602a718799ac3873a349c2b4", {
-      method: "put",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({ "my-todo": data }),
-    });
+let isSorted = false;
+//event of the sort button and unsort
+sortButton.addEventListener("click", () => {
+  if (isSorted === false) {
+    isSorted = true;
+    const newArr = [];
+    for (let j = 5; j >= 1; j--) {
+      for (let i = 0; i < taskArr.length; i++) {
+        if (Number(taskArr[i].priority) === j) {
+          newArr.push(taskArr[i]);
+        }
+      }
+    }
+    printViewSection(newArr);
+  } else {
+    isSorted = false;
+    printViewSection(taskArr);
   }
-  let taskArr = await getPersistent();
-  printViewSection(taskArr);
-  countText.innerText = JSON.stringify(taskArr.length);
+});
 
-  //event of add button
-  addButton.addEventListener("click", () => {
-    const pin = document.createElement("div"); //making the pin with random color
+//function that gets the type of arr that we have in JSON bin => arr of objects
+function printViewSection(arr) {
+  viewSection.innerText = "";
+  for (let i = 0; i < arr.length; i++) {
+    const pin = document.createElement("div");
     pin.className = "pin";
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     pin.style.backgroundColor = `#${randomColor}`;
-    const task = textInput.value;
-    if (!task.trim()) {
-      //if the input is empty - get out from the function
-      alert("Please enter your task and prioirity");
-      return;
-    }
-    textInput.value = ""; //make the input empty again
-    let priority = document.getElementById("priority-selector").value;
     const todoContainer = document.createElement("div");
     todoContainer.className = "todo-container";
-    const color = colorTask(priority); //paint the task backgorund div for each priority
-    todoContainer.style.backgroundColor = color;
-    const priorityDiv = document.createElement("div"); //making priority div
-    priorityDiv.innerHTML = priority;
+
+    const priorityDiv = document.createElement("div");
+    priorityDiv.innerHTML = arr[i].priority;
     priorityDiv.className = "todo-priority";
-    const createdAtDiv = document.createElement("div"); //making date div
-    createdAtDiv.innerHTML = new Date().toDateString();
+
+    const color = colorTask(arr[i].priority);
+    todoContainer.style.backgroundColor = color;
+
+    const createdAtDiv = document.createElement("div");
+    createdAtDiv.innerHTML = arr[i].date;
     createdAtDiv.className = "todo-created-at";
-    const textDiv = document.createElement("div"); //created task div
-    textDiv.innerHTML = task;
+
+    const textDiv = document.createElement("div");
+    textDiv.innerHTML = arr[i].text;
     textDiv.className = "todo-text";
     todoContainer.append(pin, priorityDiv, createdAtDiv, textDiv);
     viewSection.appendChild(todoContainer);
-    taskArr.push({
-      //push the task to the main arr
-      priority: priority,
-      date: new Date().toDateString(),
-      text: task,
-    });
-    setPersistent(taskArr); //set the taskArr in the JSON bin
-    countText.innerText = taskArr.length;
-  });
-
-  let isSorted = false;
-  //event of the sort button and unsort
-  sortButton.addEventListener("click", async () => {
-    if (isSorted === false) {
-      isSorted = true;
-      const newArr = [];
-      for (let j = 5; j >= 1; j--) {
-        for (let i = 0; i < taskArr.length; i++) {
-          if (Number(taskArr[i].priority) === j) {
-            newArr.push(taskArr[i]);
-          }
-        }
-      }
-      printViewSection(newArr);
-    } else {
-      isSorted = false;
-      printViewSection(taskArr);
-    }
-  });
-
-  //function that gets the type of arr that we have in JSON bin => arr of objects
-  function printViewSection(arr) {
-    viewSection.innerText = "";
-    for (let i = 0; i < arr.length; i++) {
-      const pin = document.createElement("div");
-      pin.className = "pin";
-      var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-      pin.style.backgroundColor = `#${randomColor}`;
-      const todoContainer = document.createElement("div");
-      todoContainer.className = "todo-container";
-
-      const priorityDiv = document.createElement("div");
-      priorityDiv.innerHTML = arr[i].priority;
-      priorityDiv.className = "todo-priority";
-
-      const color = colorTask(arr[i].priority);
-      todoContainer.style.backgroundColor = color;
-
-      const createdAtDiv = document.createElement("div");
-      createdAtDiv.innerHTML = arr[i].date;
-      createdAtDiv.className = "todo-created-at";
-
-      const textDiv = document.createElement("div");
-      textDiv.innerHTML = arr[i].text;
-      textDiv.className = "todo-text";
-      todoContainer.append(pin, priorityDiv, createdAtDiv, textDiv);
-      viewSection.appendChild(todoContainer);
-    }
   }
-
-  //function that get a number of prioirity and returns a color
-  function colorTask(numStr) {
-    switch (numStr) {
-      case "1":
-        return "#faa7ff";
-        break;
-      case "2":
-        return "#9fff67";
-        break;
-      case "3":
-        return "#fffe93";
-        break;
-      case "4":
-        return "#ffc76b";
-        break;
-      case "5":
-        return "#7efff3";
-        break;
-    }
-  }
-
-  //event that delete an item in the view section and JSON bin
-  document.addEventListener("click", (e) => {
-    if (e.target.className !== "pin") {
-      //if you dont click on the pin
-      return;
-    }
-    const tooltipDelete = document.getElementById("tooltip-delete");
-    e.target.parentNode.remove();
-    tooltipDelete.hidden = true;
-    savingChanges();
-  });
-
-  //event of a delete pin tooltip
-  document.addEventListener("mouseover", (e) => {
-    if (e.target.className !== "pin") {
-      return;
-    }
-    const tooltipDelete = document.getElementById("tooltip-delete");
-    tooltipDelete.style.top = e.target.getBoundingClientRect().top - 40 + "px";
-    tooltipDelete.style.left =
-      e.target.getBoundingClientRect().left - 10 + "px";
-    tooltipDelete.hidden = false;
-  });
-  //and mouseout for the tooltip
-  document.addEventListener("mouseout", (e) => {
-    if (e.target.className !== "pin") {
-      return;
-    }
-    const tooltipDelete = document.getElementById("tooltip-delete");
-    tooltipDelete.hidden = true;
-  });
-
-  //event of a edit tooltip
-  document.addEventListener("mouseover", (e) => {
-    if (e.target.className !== "todo-text") {
-      return;
-    }
-    const tooltipEdit = document.getElementById("tooltip-edit");
-    tooltipEdit.style.top = e.target.getBoundingClientRect().top - 40 + "px";
-    tooltipEdit.style.left = e.target.getBoundingClientRect().left + 0 + "px";
-    tooltipEdit.hidden = false;
-  });
-  //and mouseout for the tooltip
-  document.addEventListener("mouseout", (e) => {
-    if (e.target.className !== "todo-text") {
-      return;
-    }
-    const tooltipEdit = document.getElementById("tooltip-edit");
-    tooltipEdit.hidden = true;
-  });
-
-  //double click on .todo-text div for edit
-  document.addEventListener("dblclick", (e) => {
-    const task = e.target.innerHTML;
-    if (e.target.className !== "todo-text") {
-      return;
-    }
-    e.target.contentEditable = true;
-    //CTRL to save the edit task
-    document.addEventListener("keydown", async (event) => {
-      if (event.ctrlKey) {
-        // you cant save an empty task
-        if (e.target.innerHTML === "") {
-          e.target.innerHTML = task;
-        }
-        e.target.contentEditable = false;
-        await savingChanges();
-      }
-    });
-    e.target.innerHTML = task;
-  });
-
-  //function that saving all the tasks in JSON bin
-  async function savingChanges() {
-    let arr = document.getElementsByClassName("todo-container");
-    let newArr = [];
-    for (const task of arr) {
-      const priority = task.childNodes[1].innerHTML;
-      const date = task.childNodes[2].innerHTML;
-      const taskInner = task.childNodes[3].innerHTML;
-      newArr.push({
-        priority: priority,
-        date: date,
-        text: taskInner,
-      });
-    }
-    countText.innerText = newArr.length;
-    await setPersistent(newArr);
-    taskArr = await getPersistent();
-  }
-
-  // event of search button that paint the font of the searched task
-  searchButton.addEventListener("click", () => {
-    const searchInput = textInput.value;
-    if (!searchInput.trim()) {
-      //if the input is empty - do nothing
-      return;
-    }
-    textInput.value = "";
-    const arr = document.querySelectorAll(".todo-text");
-    for (const task of arr) {
-      if (task.innerHTML.search(searchInput) !== -1) {
-        task.style.color = "red";
-      } else {
-        task.style.color = "#444444";
-      }
-    }
-  });
 }
-main();
+
+//function that get a number of prioirity and returns a color
+function colorTask(numStr) {
+  switch (numStr) {
+    case "1":
+      return "#faa7ff";
+      break;
+    case "2":
+      return "#9fff67";
+      break;
+    case "3":
+      return "#fffe93";
+      break;
+    case "4":
+      return "#ffc76b";
+      break;
+    case "5":
+      return "#7efff3";
+      break;
+  }
+}
+
+//event that delete an item in the view section and JSON bin
+document.addEventListener("click", (e) => {
+  if (e.target.className !== "pin") {
+    //if you dont click on the pin
+    return;
+  }
+  const tooltipDelete = document.getElementById("tooltip-delete");
+  e.target.parentNode.remove();
+  tooltipDelete.hidden = true;
+  savingChanges();
+});
+
+//event of a delete pin tooltip
+document.addEventListener("mouseover", (e) => {
+  if (e.target.className !== "pin") {
+    return;
+  }
+  const tooltipDelete = document.getElementById("tooltip-delete");
+  tooltipDelete.style.top = e.target.getBoundingClientRect().top - 40 + "px";
+  tooltipDelete.style.left = e.target.getBoundingClientRect().left - 10 + "px";
+  tooltipDelete.hidden = false;
+});
+//and mouseout for the tooltip
+document.addEventListener("mouseout", (e) => {
+  if (e.target.className !== "pin") {
+    return;
+  }
+  const tooltipDelete = document.getElementById("tooltip-delete");
+  tooltipDelete.hidden = true;
+});
+
+//event of a edit tooltip
+document.addEventListener("mouseover", (e) => {
+  if (e.target.className !== "todo-text") {
+    return;
+  }
+  const tooltipEdit = document.getElementById("tooltip-edit");
+  tooltipEdit.style.top = e.target.getBoundingClientRect().top - 40 + "px";
+  tooltipEdit.style.left = e.target.getBoundingClientRect().left + 0 + "px";
+  tooltipEdit.hidden = false;
+});
+//and mouseout for the tooltip
+document.addEventListener("mouseout", (e) => {
+  if (e.target.className !== "todo-text") {
+    return;
+  }
+  const tooltipEdit = document.getElementById("tooltip-edit");
+  tooltipEdit.hidden = true;
+});
+
+//double click on .todo-text div for edit
+document.addEventListener("dblclick", (e) => {
+  const task = e.target.innerHTML;
+  if (e.target.className !== "todo-text") {
+    return;
+  }
+  e.target.contentEditable = true;
+  //CTRL to save the edit task
+  document.addEventListener("keydown", (event) => {
+    if (event.ctrlKey) {
+      // you cant save an empty task
+      if (e.target.innerHTML === "") {
+        e.target.innerHTML = task;
+      }
+      e.target.contentEditable = false;
+      savingChanges();
+    }
+  });
+  e.target.innerHTML = task;
+});
+
+//function that saving all the tasks in JSON bin
+function savingChanges() {
+  let arr = document.getElementsByClassName("todo-container");
+  taskArr = [];
+  for (const task of arr) {
+    const priority = task.childNodes[1].innerHTML;
+    const date = task.childNodes[2].innerHTML;
+    const taskInner = task.childNodes[3].innerHTML;
+    taskArr.push({
+      priority: priority,
+      date: date,
+      text: taskInner,
+    });
+  }
+  countText.innerText = taskArr.length;
+  setPersistent(taskArr);
+}
+
+// event of search button that paint the font of the searched task
+searchButton.addEventListener("click", () => {
+  const searchInput = textInput.value;
+  if (!searchInput.trim()) {
+    //if the input is empty - do nothing
+    return;
+  }
+  textInput.value = "";
+  const arr = document.querySelectorAll(".todo-text");
+  for (const task of arr) {
+    if (task.innerHTML.search(searchInput) !== -1) {
+      task.style.color = "red";
+    } else {
+      task.style.color = "#444444";
+    }
+  }
+});
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,45 @@
-const API_KEY = ""; // Assign this variable to your JSONBIN.io API key if you choose to use it.
-const DB_NAME = "my-todo";
+let todoContainers = document.getElementsByClassName("todo-container");
+console.log(todoContainers);
 
 // Gets data from persistent storage by the given key and returns it
-async function getPersistent(key) {
-  return [];
+function getPersistent() {
+  for (const todo of todoContainers) {
+    todo.hidden = true;
+  }
+  loader.hidden = false;
+  const promise = fetch(
+    "https://api.jsonbin.io/v3/b/602a718799ac3873a349c2b4/latest"
+  );
+  const loadedData = promise.then((response) => {
+    return response.json();
+  });
+  const data = loadedData.then((result) => {
+    taskArr = result.record["my-todo"];
+    loader.hidden = true;
+    for (const todo of todoContainers) {
+      todo.hidden = false;
+    }
+    countText.innerText = JSON.stringify(taskArr.length);
+    printViewSection(taskArr);
+  });
 }
 
 // Saves the given data into persistent storage by the given key.
 // Returns 'true' on success.
-async function setPersistent(key, data) {
-  return true;
+function setPersistent(data) {
+  for (const todo of todoContainers) {
+    todo.hidden = true;
+  }
+  loader.hidden = false;
+  fetch("https://api.jsonbin.io/v3/b/602a718799ac3873a349c2b4", {
+    method: "put",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ "my-todo": data }),
+  }).then((res) => {
+    loader.hidden = true;
+    for (const todo of todoContainers) {
+      todo.hidden = false;
+    }
+    return res.json();
+  });
 }
